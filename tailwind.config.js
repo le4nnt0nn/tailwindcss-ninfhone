7669de7488c416b38e7cd4bfdb055c3bfe6a106c
Antonio Cortés <ditacj78@gmail.com>
2022-02-11 16:55:47 +0100

    init web project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    theme: {
+      extend: {
+          screens: {
+            
+          },
+          keyframes: {
+         
+          },
+          animation: {
+            
+          },
+          backgroundImage: theme => ({
+              'backweb': "url('../img/back.png')"
+          })
+      },
+    },
+    variants: {
+      animation: ['responsive', 'hover', 'group-hover'],
+      animate: ['responsive', 'hover', 'group-hover'],
+      fontSize: ['responsive', 'hover', 'group-hover'],
+    }
+}
