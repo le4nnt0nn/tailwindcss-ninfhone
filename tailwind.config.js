29b091684086ba68b9e316f424421c6621cc0b3f
Antonio Cortés <ditacj78@gmail.com>
2022-02-11 18:52:15 +0100

    updated styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,31 @@
 module.exports = {
     theme: {
-      extend: {
-          screens: {
-            
-          },
-          keyframes: {
-         
-          },
-          animation: {
-            
-          },
-          backgroundImage: theme => ({
-              'backweb': "url('../img/back.png')"
-          })
-      },
+        extend: {
+            screens: {
+
+            },
+            keyframes: {
+
+            },
+            animation: {
+
+            },
+            backgroundImage: theme => ({
+                'backweb': "url('../img/back.png')"
+            }),
+            fontFamily: {
+                sen: ['Sen']
+            },
+            colors: {
+                'bluePrimary':'#639AE2',
+                'redPrimary':'#F92746',
+                'blackPrimary':'#2E2D33'
+            }
+        },
     },
     variants: {
-      animation: ['responsive', 'hover', 'group-hover'],
-      animate: ['responsive', 'hover', 'group-hover'],
-      fontSize: ['responsive', 'hover', 'group-hover'],
+        animation: ['responsive', 'hover', 'group-hover'],
+        animate: ['responsive', 'hover', 'group-hover'],
+        fontSize: ['responsive', 'hover', 'group-hover'],
     }
 }
